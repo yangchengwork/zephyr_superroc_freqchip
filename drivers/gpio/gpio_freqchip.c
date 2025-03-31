dfef0e90d911272b7da5ff4dfe6a62916bc21c70
Gump Yang <gump.yang@freqchip.com>
2025-03-31 17:30:52 +0800

    fix(gpio): 主要问题点是DT_DRV_COMPAT与设备树中的定义不同，所以之前一直没有初始化

--- drivers/gpio/gpio_freqchip.c
+++ drivers/gpio/gpio_freqchip.c
@@ -1,89 +1,94 @@
 /*
- * Copyright (c) 2021, Commonwealth Scientific and Industrial Research
- * Organisation (CSIRO) ABN 41 687 119 230.
+ * Copyright (c) 2025 Freqchip
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * This is not a real GPIO driver. It is used to instantiate struct
- * devices for the "vnd,gpio" devicetree compatible used in test code.
- */
-
-#define DT_DRV_COMPAT vnd_gpio
+#define DT_DRV_COMPAT freqchip_fr30xx_gpio
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gpio/gpio_utils.h>
 
-struct vnd_gpio_config {
+struct gpio_freqchip_config {
 	/* gpio_driver_config needs to be first */
 	struct gpio_driver_config common;
 };
 
-struct vnd_gpio_data {
+struct gpio_freqchip_data {
 	/* gpio_driver_data needs to be first */
 	struct gpio_driver_data common;
 };
 
-static int vnd_gpio_pin_configure(const struct device *port,
+static int gpio_freqchip_pin_configure(const struct device *port,
 				  gpio_pin_t pin,
 				  gpio_flags_t flags)
 {
 	return -ENOTSUP;
 }
 
-static int vnd_gpio_port_get_raw(const struct device *port,
+static int gpio_freqchip_port_get_raw(const struct device *port,
 				 gpio_port_value_t *value)
 {
 	return -ENOTSUP;
 }
 
-static int vnd_gpio_port_set_masked_raw(const struct device *port,
+static int gpio_freqchip_port_set_masked_raw(const struct device *port,
 					gpio_port_pins_t mask,
 					gpio_port_value_t value)
 {
+	printf("%s\n", __func__);
 	return -ENOTSUP;
 }
 
-static int vnd_gpio_port_set_bits_raw(const struct device *port,
+static int gpio_freqchip_port_set_bits_raw(const struct device *port,
 				      gpio_port_pins_t pins)
 {
+	printf("%s\n", __func__);
 	return -ENOTSUP;
 }
 
-static int vnd_gpio_port_clear_bits_raw(const struct device *port,
+static int gpio_freqchip_port_clear_bits_raw(const struct device *port,
 					gpio_port_pins_t pins)
 {
+	printf("%s\n", __func__);
 	return -ENOTSUP;
 }
 
-static int vnd_gpio_port_toggle_bits(const struct device *port,
+static int gpio_freqchip_port_toggle_bits(const struct device *port,
 				     gpio_port_pins_t pins)
 {
+	printf("%s\n", __func__);
 	return -ENOTSUP;
 }
 
-static DEVICE_API(gpio, vnd_gpio_api) = {
-	.pin_configure = vnd_gpio_pin_configure,
-	.port_get_raw = vnd_gpio_port_get_raw,
-	.port_set_masked_raw = vnd_gpio_port_set_masked_raw,
-	.port_set_bits_raw = vnd_gpio_port_set_bits_raw,
-	.port_clear_bits_raw = vnd_gpio_port_clear_bits_raw,
-	.port_toggle_bits = vnd_gpio_port_toggle_bits,
+static DEVICE_API(gpio, gpio_freqchip_api) = {
+	.pin_configure = gpio_freqchip_pin_configure,
+	.port_get_raw = gpio_freqchip_port_get_raw,
+	.port_set_masked_raw = gpio_freqchip_port_set_masked_raw,
+	.port_set_bits_raw = gpio_freqchip_port_set_bits_raw,
+	.port_clear_bits_raw = gpio_freqchip_port_clear_bits_raw,
+	.port_toggle_bits = gpio_freqchip_port_toggle_bits,
 };
 
-#define VND_GPIO_INIT(n)						\
-	static const struct vnd_gpio_config vnd_gpio_config_##n = {	\
+static int gpio_freqchip_init(const struct device *dev) {
+    printf("FreqChip GPIO Driver Initialized\n");
+    return 0;
+}
+
+#define GPIO_FREQCHIP_INIT(n)						\
+	static const struct gpio_freqchip_config gpio_freqchip_config_##n = {	\
 		.common = {						\
 			.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_DT_INST(n), \
 		},							\
 	};								\
 									\
-	static struct vnd_gpio_data vnd_gpio_data_##n;			\
+	static struct gpio_freqchip_data gpio_freqchip_data_##n;			\
 									\
-	DEVICE_DT_INST_DEFINE(n, NULL, NULL, &vnd_gpio_data_##n,	\
-			      &vnd_gpio_config_##n, POST_KERNEL,	\
+	DEVICE_DT_INST_DEFINE(n, gpio_freqchip_init, NULL, &gpio_freqchip_data_##n,	\
+			      &gpio_freqchip_config_##n, POST_KERNEL,	\
 			      CONFIG_GPIO_INIT_PRIORITY,		\
-			      &vnd_gpio_api);
+			      &gpio_freqchip_api);
 
-DT_INST_FOREACH_STATUS_OKAY(VND_GPIO_INIT)
+DT_INST_FOREACH_STATUS_OKAY(GPIO_FREQCHIP_INIT)
+
+// DEVICE_DT_INST_DEFINE(n, gpio_freqchip_init, ...);
