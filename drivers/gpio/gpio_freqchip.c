5e60b60798913f9de08ad2307d3e75d8f6309c0b
Gump Yang <gump.yang@freqchip.com>
2025-04-01 17:02:58 +0800

    perf(命名): 将GPIO的定义从fr30xx改为freqchip

--- drivers/gpio/gpio_freqchip.c
+++ drivers/gpio/gpio_freqchip.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define DT_DRV_COMPAT freqchip_fr30xx_gpio
+#define DT_DRV_COMPAT freqchip_freqchip_gpio
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gpio/gpio_utils.h>
